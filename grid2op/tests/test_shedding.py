9c0078eabb8d5afc7633ead41bf35153522022ca
Xavier Weiss <xavierw@kth.se>
2024-11-05 08:53:33 +0100

    Add: Unit Tests
    Signed-off-by: Xavier Weiss <xavierw@kth.se>

--- grid2op/tests/test_shedding.py
+++ grid2op/tests/test_shedding.py
@@ -8,49 +8,88 @@ class TestShedding(unittest.TestCase):
     def setUp(self) -> None:
         super().setUp()
         p = Parameters()
-        p.ALLOW_SHEDDING = True
         p.MAX_SUB_CHANGED = 5
         with warnings.catch_warnings():
             warnings.filterwarnings("ignore")
-            self.env = grid2op.make("rte_case5_example", param=p, test=True)
+            self.env = grid2op.make("rte_case5_example", param=p,
+                                    allow_shedding=True, test=True)
+            self.env.set_id("00") # Reproducibility
+        self.load_lookup = {name:i for i,name in enumerate(self.env.name_load)}
+        self.gen_lookup = {name:i for i,name in enumerate(self.env.name_gen)}
 
     def tearDown(self) -> None:
         self.env.close()
 
     def test_shedding_parameter_is_true(self):
-        assert self.env.parameters.ALLOW_SHEDDING is True
+        assert hasattr(self.env, "allow_shedding")
+        assert self.env.allow_shedding is True
 
     def test_shed_single_load(self):
         # Check that a single load can be shed
-        pass
+        load_idx = self.load_lookup["load_4_2"]
+        load_pos = self.env.load_pos_topo_vect[load_idx]
+        act = self.env.action_space({
+            "set_bus": [(load_pos, -1)]
+        })
+        obs, _, done, info = self.env.step(act)
+        assert not done
+        assert info["is_illegal"] is False
+        assert obs.topo_vect[load_pos] == -1
 
     def test_shed_single_generator(self):
         # Check that a single generator can be shed
-        pass
+        gen_idx = self.gen_lookup["gen_0_0"]
+        gen_pos = self.env.gen_pos_topo_vect[gen_idx]
+        act = self.env.action_space({
+            "set_bus": [(gen_pos, -1)]
+        })
+        obs, _, done, info = self.env.step(act)
+        assert not done
+        assert info["is_illegal"] is False
+        assert obs.topo_vect[gen_pos] == -1
 
     def test_shed_multiple_loads(self):
         # Check that multiple loads can be shed at the same time
-        pass
-
-    def test_shed_multiple_generators(self):
-        # Check that multiple generators can be shed at the same time
-        pass
+        load_idx1 = self.load_lookup["load_4_2"]
+        load_idx2 = self.load_lookup["load_3_1"]
+        load_pos1 = self.env.load_pos_topo_vect[load_idx1]
+        load_pos2 = self.env.load_pos_topo_vect[load_idx2]
+        act = self.env.action_space({
+            "set_bus": [(load_pos1, -1), (load_pos2, -1)]
+        })
+        obs, _, done, info = self.env.step(act)
+        assert not done
+        assert info["is_illegal"] is False
+        assert obs.topo_vect[load_pos1] == -1
+        assert obs.topo_vect[load_pos2] == -1
 
     def test_shed_load_and_generator(self):
         # Check that load and generator can be shed at the same time
-        pass
-
-    def test_shed_energy_storage(self):
-        # Check that energy storage device can be shed successfully
-        pass
-
-    def test_shedding_appears_in_observation(self):
-        # Check that observation contains correct information about shedding
-        pass
+                # Check that multiple loads can be shed at the same time
+        load_idx = self.load_lookup["load_4_2"]
+        gen_idx = self.gen_lookup["gen_0_0"]
+        load_pos = self.env.load_pos_topo_vect[load_idx]
+        gen_pos = self.env.gen_pos_topo_vect[gen_idx]
+        act = self.env.action_space({
+            "set_bus": [(load_pos, -1), (gen_pos, -1)]
+        })
+        obs, _, done, info = self.env.step(act)
+        assert not done
+        assert info["is_illegal"] is False
+        assert obs.topo_vect[load_pos] == -1
+        assert obs.topo_vect[gen_pos] == -1
 
     def test_shedding_persistance(self):
         # Check that components remains disconnected if shed
-        pass
+        load_idx = self.load_lookup["load_4_2"]
+        load_pos = self.env.load_pos_topo_vect[load_idx]
+        act = self.env.action_space({
+            "set_bus": [(load_pos, -1)]
+        })
+        _ = self.env.step(act)
+        obs, _, done, _ = self.env.step(self.env.action_space({}))
+        assert not done
+        assert obs.topo_vect[load_pos] == -1
 
 if __name__ == "__main__":
     unittest.main()
