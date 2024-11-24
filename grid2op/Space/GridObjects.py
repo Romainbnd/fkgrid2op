0666b398c4e60b04a2974864fa69161807e5c0d5
Xavier Weiss <xavierw@kth.se>
2024-11-24 09:36:33 +0100

    Debug: Fix nested f-string in GridObjects
    Signed-off-by: Xavier Weiss <xavierw@kth.se>

--- grid2op/Space/GridObjects.py
+++ grid2op/Space/GridObjects.py
@@ -4210,7 +4210,7 @@ class GridObjects:
             elif dict_["detachment_is_allowed"] == "False":
                 cls.detachment_is_allowed = False
             else:
-                raise ValueError(f"'detachment_is_allowed' (value: {dict_["detachment_is_allowed"]}'')" +
+                raise ValueError(f"'detachment_is_allowed' (value: {dict_['detachment_is_allowed']}'')" +
                                   "could not be converted to Boolean ")
         else: # Compatibility for older versions
             cls.detachment_is_allowed = DEFAULT_ALLOW_DETACHMENT
